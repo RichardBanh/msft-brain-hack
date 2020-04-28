d7407d078730504438382a579a2e3b6eedd6d111
sdavidson90 <s.davidson90@hotmail.ca>
2020-04-28 16:41:04 -0700

    navbar css done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <div className="header">
+          <Link to="/" className="header__left"></Link>
+          <div className="header__right">
+            <Link to="" className="header__right-link">
+              <h3 className="header__right-item">Explore</h3>
+            </Link>
+            <Link to="" className="header__right-link">
+              <h3 className="header__right-item">My Challenges</h3>
+            </Link>
+            <Link to="" className="header__right-link">
+              <h3 className="header__right-item">My Profile</h3>
+            </Link>
+            <Link to="" className="header__right-link">
+              <h3 className="header__right-item">Sign Out</h3>
+            </Link>
+          </div>
+        </div>
         <Categories />
         <Switch>
           <Route exact path="/"></Route>
