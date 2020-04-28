a7b0ad7c61e1b279e408d566c0f1841bf204ebb9
richardbanh <rbanh@me.com>
2020-04-28 16:12:56 -0700

    done adding data models

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import Homepage from './Components/Pages/Homepage'
+import Homepage from "./Components/Pages/Homepage";
 import "./Styles/main.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <Link to="/" className="header__left">
+          <img className="header__left-home" alt="logo" />
+        </Link>
+        <div className="header__right">
+          <Link to="" className="header__right-link">
+            <h3 className="header__right-item">Explore</h3>
+          </Link>
+          <Link to="" className="header__right-link">
+            <h3 className="header__right-item">My Challenges</h3>
+          </Link>
+          <Link to="" className="header__right-link">
+            <h3 className="header__right-item">My Profile</h3>
+          </Link>
+          <Link to="" className="header__right-link">
+            <h3 className="header__right-item">Sign Out</h3>
+          </Link>
+        </div>
+      </div>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/'>
-            <Homepage/>
+          <Route exact path="/">
+            <Homepage />
           </Route>
         </Switch>
       </BrowserRouter>
@@ -18,7 +37,6 @@ function App() {
 }
 
 export default App;
-
 
 //code snippet for writing data
 //sorting function?
--- src/Components/CardLoop.jsx
+++ src/Components/CardLoop.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-function Card() {
+function CardLoop() {
 
     return( <div></div>);
     }
     
-    export default Card
+    export default CardLoop
