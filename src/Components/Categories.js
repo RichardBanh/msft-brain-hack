f3b920bc041c73eb1cc675f3ab7cb40545cda11e
sdavidson90 <s.davidson90@hotmail.ca>
2020-04-28 16:06:11 -0700

    process card css complete

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Coffee from "../Assets/Cards/SVG/Coffee.svg";
+import Catnap from "../Assets/Cards/SVG/Catnap.svg";
 import Progress from "../Assets/Cards/SVG/Progress.svg";
 
 const Categories = () => {
   return (
     <div className="card">
-      <img className="card__image" src={Coffee} alt="Coffee" />
+      <img className="card__image" src={Catnap} alt="Coffee" />
       <div className="card__bottom">
         <h3 className="card__bottom-title">Sleep at 11:00PM Everyday</h3>
         <h3 className="card__bottom-ending">Ends in 7 days</h3>
